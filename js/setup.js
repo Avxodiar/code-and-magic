a3e594e168296e74614401cf26a66dd37fc095e2
KHaritov Andrei <avxodiar@gmail.com>
2019-07-16 11:40:25 +0300

    Окно настроки сделон перетаскиваемым

--- js/setup.js
+++ js/setup.js
@@ -83,7 +83,8 @@ function evtCloseSetup() {
   setup.classList.add('hidden');
   document.removeEventListener('keydown', evtCloseSetupEscPress );
   document.removeEventListener('keydown', evtFormSubmitEnterPress );
-  setupWizard.removeEventListener('click', evtWizardCoatClickColorChange );
+  setupWizard.removeEventListener('click', evtWizardColorChange );
+  setupFireball.removeEventListener('click', evtWizardColorChange );
 }
 /* Показ по нажатию ENTER */
 function evtShowSetupEnterPress(evt) {
@@ -114,6 +115,63 @@ function evtFormSubmitEnterPress(evt) {
     else  alert('submit');
   }
 }
+
+// перемещение блока настроек за иконку пользователя в настройках
+var dialogHandler = setup.querySelector('.upload');
+var movingDiv = setup.querySelector('.setup-moving');
+
+dialogHandler.addEventListener('mousedown',onSetupMoving);
+movingDiv.addEventListener('mousedown',onSetupMoving);
+
+function onSetupMoving (evt) {
+  evt.preventDefault();
+  setupUserName.blur();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    }
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    }
+    setup.style.top = ( setup.offsetTop - shift.y) + 'px';
+    setup.style.left = ( setup.offsetLeft - shift.x) + 'px';
+    movingDiv.style.cursor = 'move';
+  }
+
+  var onMouseUp = function(upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+    movingDiv.style.cursor = '';
+
+    if (dragged) {
+      var onClickPreventDefault = function(evt) {
+        evt.preventDefault();
+        dialogHandler.removeEventListener('click', onClickPreventDefault);
+      }
+      dialogHandler.addEventListener('click', onClickPreventDefault);
+    }
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+}
+
 
 function evtWizardColorChange(evt) {
   var wizardProp = '';
